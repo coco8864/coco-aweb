6150063eab74e97a25dce0d9d3579d13403610fe
coco8864 <coco8864@aroma.ocn.ne.jp>
2011-05-30 14:05:17 +0000

    queuelet 1.2.0対応

--- pack/phantom_tmpl/bin/run.bat
+++ pack/phantom_tmpl/bin/run.bat
@@ -4,5 +4,5 @@ set QUEUELET_HOME=X:/your/install/phantom
 set PH_HOME=%QUEUELET_HOME%/ph
 set DEBUG_FLAG=
 rem set DEBUG_FLAG=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address="1234"
-"%JAVA_HOME%\bin\java" %DEBUG_FLAG% -Xms128m -Xmx128m -DQUEUELET_HOME=%QUEUELET_HOME%  -DPH_HOME=%PH_HOME% -jar %QUEUELET_HOME%\bin\queuelet-boot-1.1.0.jar phantom.xml %1 %2 %3 %4
+"%JAVA_HOME%\bin\java" %DEBUG_FLAG% -DQUEUELET_HOME=%QUEUELET_HOME% -DPH_HOME=%PH_HOME% -jar %QUEUELET_HOME%\bin\queuelet-boot-1.2.0.jar phantomd.xml %1 %2 %3 %4
 
--- pack/phantom_tmpl/bin/run.sh
+++ pack/phantom_tmpl/bin/run.sh
@@ -3,5 +3,5 @@ JAVA_HOME=/your/java/home
 QUEUELET_HOME=/your/install/phantom
 PH_HOME=${QUEUELET_HOME}/ph
 DEBUG_FLAG=
-#DEBUG_FLAG=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address="1234"
-${JAVA_HOME}/bin/java ${DEBUG_FLAG} -Xms128m -Xmx128m -DPH_HOME=${PH_HOME} -DQUEUELET_HOME=${QUEUELET_HOME} -jar ${QUEUELET_HOME}/bin/queuelet-boot-1.1.0.jar phantom.xml $1 $2 $3 $4
+#DEBUG_FLAG=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,address=1234
+${JAVA_HOME}/bin/java ${DEBUG_FLAG} -DPH_HOME=${PH_HOME} -DQUEUELET_HOME=${QUEUELET_HOME} -jar ${QUEUELET_HOME}/bin/queuelet-boot-1.2.0.jar phantomd.xml $1 $2 $3 $4
