84297fc558c2cb5cefc89a0c53846a3c82aae323
coco8864 <coco8864@aroma.ocn.ne.jp>
2011-06-18 20:43:27 +0000

    CallSchedulerを修正

--- src/main/java/naru/aweb/robot/CallScheduler.java
+++ src/main/java/naru/aweb/robot/CallScheduler.java
@@ -12,7 +12,7 @@ import naru.aweb.http.WebClientHandler;
 public class CallScheduler extends PoolBase{
 	private static Logger logger = Logger.getLogger(CallScheduler.class);
 	
-	private WebClientHandler handler;
+	private WebClientHandler handler;//一旦設定されたらrecycleされるまで変更されない
 	//異常のシミュレート,時間は絶対時間で設定する
 	//1)connectして送信開始するまでの時間 (0)
 	//2)送信するrequestLine長 0-実requestLine長(実requestLine長)将来検討
@@ -27,64 +27,72 @@ public class CallScheduler extends PoolBase{
 	private long headerLength;
 	private long bodyTime;
 	private long bodyLength;
-	private WriteScheduler prevScheduler;
+	private WriteScheduler headerScheduler;
+	private WriteScheduler bodyScheduler;
 	
-	public void setup(WebClientHandler handler,long headerTime,long headerLength,long bodyTime,long bodyLength){
-		setHandler(handler);
+	public static CallScheduler create(WebClientHandler handler,long headerTime,long headerLength,long bodyTime,long bodyLength){
+		CallScheduler scheduler=(CallScheduler) PoolManager.getInstance(CallScheduler.class);
+		scheduler.setup(handler, headerTime, headerLength, bodyTime, bodyLength);
+		return scheduler;
+	}
+	
+	private void setup(WebClientHandler handler,long headerTime,long headerLength,long bodyTime,long bodyLength){
+		handler.setScheduler(this);
+		this.handler=handler;
 		this.headerTime=headerTime;
 		this.headerLength=headerLength;
 		this.bodyTime=bodyTime;
 		this.bodyLength=bodyLength;
 	}
-
-	private void setPrevScheduler(WriteScheduler prevScheduler){
-		if(prevScheduler!=null){
-			prevScheduler.ref();
-		}
-		if(this.prevScheduler!=null){
-			this.prevScheduler.unref();
-		}
-		this.prevScheduler=prevScheduler;
-	}
-	
-	private void setHandler(WebClientHandler handler){
-		if(handler!=null){
-			handler.ref();
-		}
-		if(this.handler!=null){
-			this.handler.unref();
-		}
-		this.handler=handler;
-	}
 	
 	public void recycle() {
-		setHandler(null);
-		setPrevScheduler(null);
+		handler=null;
+		if(headerScheduler!=null){
+			headerScheduler.unref();
+			headerScheduler=null;
+		}
+		if(bodyScheduler!=null){
+			bodyScheduler.unref();
+			bodyScheduler=null;
+		}
 		super.recycle();
 	}
 	
 	public void cancel(){
-		if(prevScheduler!=null){
-			prevScheduler.cancel();
-			setPrevScheduler(null);
+		if(headerScheduler!=null){
+			headerScheduler.cancel();
 		}
-		setHandler(null);
+		if(bodyScheduler!=null){
+			bodyScheduler.cancel();
+		}
 	}
 	
 	public void scheduleWrite(String userContext,ByteBuffer[] buffer){
-		long writeTime=0;
-		long writeLength=0;
 		if(userContext==WebClientHandler.CONTEXT_HEADER){
-			writeTime=headerTime;
-			writeLength=headerLength;
+			if(headerScheduler!=null){
+				throw new UnsupportedOperationException("CallScheduler not support header multi write ");
+			}
+			headerScheduler=WriteScheduler.create(handler,userContext, buffer,headerTime, headerLength, null);
 		}else if(userContext==WebClientHandler.CONTEXT_BODY){
-			writeTime=bodyTime;
-			writeLength=bodyLength;
+			if(bodyScheduler!=null){
+				throw new UnsupportedOperationException("CallScheduler not support body multi write ");
+			}
+			bodyScheduler=WriteScheduler.create(handler,userContext, buffer,bodyTime, bodyLength, headerScheduler);
 		}else{
 			throw new IllegalArgumentException("unknown userContext."+userContext);
 		}
-		WriteScheduler scheduler=(WriteScheduler) PoolManager.getInstance(WriteScheduler.class);
-		scheduler.scheduleWrite(handler,userContext, buffer, writeTime, writeLength,  prevScheduler);
-		setPrevScheduler(scheduler);
+	}
+	
+	public long getHeaderActualWriteTime(){
+		if(headerScheduler==null){
+			return -1;
+		}
+		return headerScheduler.getActualWriteTime();
+	}
+	public long getBodyActualWriteTime(){
+		if(bodyScheduler==null){
+			return -1;
+		}
+		return bodyScheduler.getActualWriteTime();
 	}
 }
